7406205ca6cd3d40a5ab653b61a92d301558baf8
shorouq.zahra <shorouq.zahra@ri.se>
2025-11-26 13:55:19 +0100

    wip

--- src/transformers/models/gpt2_moe/configuration_gpt2moe.py
+++ src/transformers/models/gpt2_moe/configuration_gpt2moe.py
@@ -45,11 +45,6 @@ class GPT2MoEConfig(PretrainedConfig):
         attn_pdrop=0.1,
         layer_norm_epsilon=1e-5,
         initializer_range=0.02,
-        summary_type="cls_index",
-        summary_use_proj=True,
-        summary_activation=None,
-        summary_proj_to_labels=True,
-        summary_first_dropout=0.1,
         scale_attn_weights=True,
         use_cache=True,
         bos_token_id=1,
@@ -75,11 +70,6 @@ class GPT2MoEConfig(PretrainedConfig):
         self.attn_pdrop = attn_pdrop
         self.layer_norm_epsilon = layer_norm_epsilon
         self.initializer_range = initializer_range
-        self.summary_type = summary_type
-        self.summary_use_proj = summary_use_proj
-        self.summary_activation = summary_activation
-        self.summary_first_dropout = summary_first_dropout
-        self.summary_proj_to_labels = summary_proj_to_labels
         self.scale_attn_weights = scale_attn_weights
         self.use_cache = use_cache
         self.scale_attn_by_inverse_layer_idx = scale_attn_by_inverse_layer_idx
