58de9a3b89d727b7bfd4a3d9455689cedfa17b3a
shorouq.zahra <shorouq.zahra@ri.se>
2025-11-27 16:43:03 +0100

    implement experts as original gpt2 mlp

--- src/transformers/models/gpt2_moe/modeling_gpt2moe.py
+++ src/transformers/models/gpt2_moe/modeling_gpt2moe.py
@@ -116,10 +116,7 @@ class GPT2SparseMoEBlock(nn.Module):
         self.k = config.top_k_expert
 
         self.experts = nn.ModuleList(
-            [
-                GPT2MoEFeedForward(intermediate_size, config)
-                for _ in range(self.num_expert)
-            ]
+            [GPT2MLP(intermediate_size, config) for _ in range(self.num_expert)]
         )
 
         self.gating_network = nn.Linear(
